5a5abfcf54d9f681351a2dcf9ab22870eeff1ebe
Rachelnk <rachelnk94@gmail.com>
2019-02-08 14:06:31 +0300

    [starts #163683137] adds the get request method for a specific office

--- offices_view.py
+++ offices_view.py
@@ -33,3 +33,18 @@ def get_offices():
         "status":404,
         "error": "No office data found"
     }),404)
+
+@offices_bp.route("/offices/<int:office_id>", methods =['GET'])
+def get_specific_offices(office_id):
+
+    political_office = OfficesModel.get_specific_offices(office_id)
+
+    if political_office:
+        return make_response(jsonify({
+            "status": 200,
+            "data": political_office
+        }), 200)
+    return make_response(jsonify({
+        "status": 404,
+        "error": "The political office does not exist"
+    }), 404)
