0fbf891b5d1dea5192e927ad673516cb4ad5e3ee
Rachelnk <rachelnk94@gmail.com>
2019-02-09 11:18:57 +0300

    [finishes #163683137] adds office model

---
+++ app/api/v1/models/office_models.py
@@ -0,0 +1,47 @@
+politicaloffice = []
+class OfficesModel():
+    
+    id = 0
+    name = ""
+    office_type = ""
+
+    def __init__(self):
+       self.politicaloffice = []
+       
+    def save_office(self):
+        office = {
+            'id':len(self.politicaloffice) + 1,
+            'name':self.name,
+            "office_type":self.office_type
+        }
+
+        self.politicaloffice.append(office)
+        #return dict(status=201, data=office)
+        return office
+     
+
+
+    #fetching a list of all the offices
+    @staticmethod
+    def get_all_offices():
+        return [vars(office) for office in politicaloffice]
+
+    #fetching a specific office
+    @staticmethod
+    def get_specific_offices(id):
+        return [vars(office) for office in politicaloffice if office.id == id]
+    
+    
+    def delete_politicaloffice(self,offices_id):
+        try:
+            for office in self.politicaloffice:
+                if office.id == offices_id:
+                    office.pop(office)
+                    return True
+        except:
+            return False
+    
+ 
+
+
+
