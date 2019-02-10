38bcc8f524d0c268d444d005b3ee0b00c1ce8e22
Rachelnk <rachelnk94@gmail.com>
2019-02-10 08:30:28 +0300

    [finishes #163682506] adds the unit tests for political party inputs

--- app/tests/test_party_input.py
+++ app/tests/test_party_input.py
@@ -1,50 +1,64 @@
+"""Test Class for party views and model"""
 import json
 import unittest
-from Forum.app.api.v1.models.parties.parties_models import PartiesModel
-from Forum.tests.v1.__init__ import BaseTest
-from Forum.app.__int__ import create_app
+from Forum.tests.v1.base_test import BaseTestCase
+from Forum.tests.v1.helper_methods import create_party
 
-class PartyData:
-    def setUp(self):
-        self.app = create_app
-        self.client = self.app.test_client()
-        Party = PartiesModel()
-        party1 = {
+
+class TestParty(BaseTestCase):
+    """docstring for TestParty"""
+    politicalparty= {
             "id":4,
             "name":"office1",
             "logoUrl":"logoUrl1",
             "hqAddress":"hqAddress1"
 
         }
-        party2 = {
+    edited_politicalparty = {
             "id":9,
             "name":"office2",
             "logoUrl":"logoUr2",
             "hqAddress":"hqAddress2"
         }
-class BaseOfficeClass(PartyData,BaseTest):
-    """sets up the path for endpoints"""
-    path ="/api/v1/parties"
 
-class TestEndpoints(PartyData, unittest.TestCase):
-    
-    def test_create_parties(self):
-        resp = self.client.post(path, data=json.dumps(self.politicaloffice1), content_type='application/json')
-        self.assertEqual(resp.status_code, 201)
-    def test_get_all_parties(self):
-        """tests endpoint to get all parties"""
-        resp = self.client.get(path, content_type='application/json')
+    def setUp(self):
+        super(TestParty, self).setUp()
+
+    def test_posting_a_party(self):
+        """Test for creating a new party"""
+        resp = create_party(self, politicalparty)
         self.assertEqual(resp.status_code, 200)
-    
-    def test_get_specific_parties(self):
-        """tests the endpoint to get a specific office"""        
-        post = self.client.post(path='', data=json.dumps(self.politicaloffice1), content_type='application/json')
-        int_id = int(post.json['blog_id'])
-        path = '{}'.format(int_id)
+
+    def test_getting_all_parties(self):
+        """Test for getting all parties"""
+        resp = self.client.get(path='/api/v1/parties/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_getting_a_single_party(self):
+        """Test for getting a single party"""
+        post = create_party(self, politicalparty)
+        int_id = int(post.json['data'][-1]['id'])
+        path = '/api/v1/parties/{}'.format(int_id)
         response = self.client.get(path, content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        
+
+    def test_editing_a_party(self):
+        """Test for editing a party"""
+        post = create_party(self, politicalparty)
+        int_id = int(post.json['data'][-1]['id'])
+        path = '/api/v1/parties/{}'.format(int_id)
+        response = self.client.put(
+            path, data=json.dumps(edited_politicalparty), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+
+    def test_deleting_a_party(self):
+        """Test for deleting a party"""
+        post = create_party(self, politicalparty)
+        int_id = int(post.json['data'][-1]['id'])
+        path = '/api/v1/parties/{}'.format(int_id)
+        response = self.client.delete(path, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
-
